328701f8d42e41e6677c33ec615b13a7f55da7ea
deborasamara <deborasamara.ro@gmail.com>
2025-01-03 11:55:27 -0300

    feat(Post): implementação do código da página de  publicação aberta

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,12 +1,28 @@
+import Comentario from "../../components/Comentario";
+import PostAberto from "../../components/PostAberto";
+
 function Post() {
   return (
     <>
-      <div
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "90vh" }}
-      >
-        
-      </div>
+        <nav className="br-breadcrumb" aria-label="Breadcrumbs">
+          <ol className="crumb-list" role="list">
+            <li className="crumb home">
+              <a className="br-button circle" href="javascript:void(0)">
+                <span className="sr-only">Página inicial</span>
+                <i className="fas fa-home"></i>
+              </a>
+            </li>
+            <li className="crumb" data-active="active">
+              <i className="icon fas fa-chevron-right"></i>
+              <span tabIndex="0" aria-current="page">
+                Post
+              </span>
+            </li>
+          </ol>
+        </nav>
+        <h2>Post</h2>
+        <PostAberto></PostAberto>
+        <Comentario></Comentario>
     </>
   );
 }
